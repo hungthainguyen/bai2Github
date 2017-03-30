d6e7692892fc765705261b4aa7bdda433c4c6be2
duongvan <banmaixanh8486@gmail.com>
2017-03-30 19:20:00 +0700

    Commitchanged

---
+++ app/src/main/java/helloworld/cokhi/duongvan/bai2github/LoginActivity.java
@@ -0,0 +1,12 @@
+package helloworld.cokhi.duongvan.bai2github;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by duongvan on 30/03/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity
+
+{
+}
